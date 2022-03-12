9c81cdb5b0964c3c14bc6ef7793547e43dfa81b7
ernes raikulov <eraykulov@gmail.com>
2022-03-12 20:42:37 +0600

    add button

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,8 +4,6 @@ import { FaFacebookSquare, FaTelegram } from "react-icons/fa";
 import { RiWhatsappFill } from "react-icons/ri";
 import { AiFillInstagram } from "react-icons/ai";
 
-
-
 const Card = ({ item }) => {
   return (
     <>
@@ -30,12 +28,11 @@ const Card = ({ item }) => {
                 </div>
 
                 <ul className="social-icons">
-                <FaFacebookSquare size={30} className="icons" />
-                <FaTelegram size={30} className="icons" />
-                <RiWhatsappFill size={32} className="icons" />
-                <AiFillInstagram size={32} className="icons"  />
-              </ul>
-
+                  <FaFacebookSquare size={30} className="icons" />
+                  <FaTelegram size={30} className="icons" />
+                  <RiWhatsappFill size={32} className="icons" />
+                  <AiFillInstagram size={32} className="icons" />
+                </ul>
               </div>
             );
           })}
