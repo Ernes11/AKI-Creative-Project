0b4f8f179334799ce2a7bf286929eef2b7c32782
ranbu171 <shulzhenko.dm@gmail.com>
2022-03-15 18:24:46 +0600

    added JoinUs page

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -1,29 +1,125 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import CustomButton from "./button/CustomButton";
+import '../sass/joinUs.scss';
+import Button from "@mui/material/Button";
+import JoinUsModal from './JoinUsModal/JoinUsModal';
+
+
+const inputs = [
+  {
+    placeholder: "Полное наименование компании/ИП",
+    logoDownload: false,
+    required: true,
+  },
+  {
+    placeholder: "ФИО руководителя компании/ИП",
+    logoDownload: false,
+    required: true,
+  },
+  {
+    placeholder: "Должность руководителя компании/ИП",
+    logoDownload: false,
+    required: false,
+  },
+  {
+    placeholder: "Дата регистрации компании/ИП",
+    logoDownload: false,
+    required: true,
+  },
+  {
+    placeholder: "Количество сотрудников компании/ИП",
+    logoDownload: false,
+    required: false,
+  },
+  {
+    placeholder: "Сайт компании/ИП",
+    logoDownload: false,
+    required: false,
+  },
+  {
+    placeholder: "Сылки на социальные страницы компании/ИП (Instagram, FaceBook, Twitter, Tik-Tok и т.д.)",
+    logoDownload: false,
+    required: false,
+  },
+  {
+    placeholder: "У вас есть логотип?",
+    logoDownload: true,
+    required: true,
+  },
+  {
+    placeholder: "К какое креативной отрасли вы себя относите?",
+    logoDownload: false,
+    required: true,
+  },
+  {
+    placeholder: "Основные направления деятельности",
+    logoDownload: false,
+    required: true,
+  }
+]
 
 function JoinUs() {
+  const [logoUrl, setLogoUrl] = useState({});
+  const logoInput = useRef();
+  const [ modalStyle, setModalStyle ] = useState('none');
+
+  function modal (style) {
+    return (
+      <JoinUsModal display={style} setDisplay ={setModalStyle}/>
+
+    )
+  }
+
   return (
-    <div className="event">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+    <main className="joinUs">
+      {modal(modalStyle)}
+      <div className="container">
+        <h1>Заполните заявку, чтобы стать членом АКИ</h1>
+
+        <form className="form-to-join" encType="multipart/form-data" method="post">
+          {inputs.map((input, index) => {
+            return (
+              input.logoDownload
+              ?
+              <div className="logo">
+                <p>{`Логотип: ${logoUrl.name? logoUrl.name : '.../'}`}</p>
+                <Button
+                variant="text"
+                component="label"
+                >
+                  Загрузить логотип
+                  <input ref={logoInput}
+                    type="file"
+                    hidden
+                    onChange={(e) => setLogoUrl(e.target.files[0])}
+                  />
+                </Button>
+              </div>
+              :
+              <label className={input.required? 'required' : ''} htmlFor={`input${index}`}>
+                <input
+                key={index}
+                type='text'
+                name ={`input${index}`}
+                size={input.placeholder.length}
+                required={input.required}
+                placeholder={input.placeholder}
+                readOnly={input.logoDownload} />
+              </label>
+            )
+          })}
+          <div className="form-contacts">
+            <label className="required" htmlFor="email">
+              <input type="mail" name="email" required placeholder="Почта"/>
+            </label>
+            <label className="required" htmlFor="phone">
+              <input type="text" name="phone" required placeholder="Телефон" />
+            </label>
           </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">JoinUs</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
+        </form>
+        <CustomButton type='contained' onClick={() => setModalStyle('flex')}>отправить</CustomButton>
       </div>
-    </div>
+    </main>
   );
 }
 
