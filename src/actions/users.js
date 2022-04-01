76d4e8a1570784d2fdcab92277e86f5a9a2ac246
ernes raikulov <eraykulov@gmail.com>
2022-04-01 17:17:18 +0600

    change api

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,18 +1,34 @@
 import axios from "axios";
 
+// export const loadUsers = () => {
+//   return async (dispatch) => {
+//     try {
+//       const users = await axios.get("./users.json");
+//       return dispatch(setUsers(users.data));
+//     } catch (error) {
+//       console.log("error:", error);
+//     }
+//   };
+// };
+
+// export const setUsers = (users) => ({
+//   type: "SET_USERS",
+//   users,
+// });
+
 export const loadUsers = () => {
   return async (dispatch) => {
     try {
-      const users = await axios.get("./users.json");
-      return dispatch(setUsers(users.data));
+      const users = await axios.get(`https://creative.kg/api/members/`);
+      return dispatch(setUsers(users.data.results));
+
     } catch (error) {
-      console.log("error:", error);
+      console.log('error:', error);
     }
   };
 };
 
 export const setUsers = (users) => ({
-  type: "SET_USERS",
-  users,
+  type: 'SET_USERS',
+  users
 });
-
