bd67a0486e7bbd09d70fadc01d606f34ac0ddabc
ernes raikulov <eraykulov@gmail.com>
2022-04-03 12:00:39 +0600

    add axios API

--- src/components/card/CoFounder.jsx
+++ src/components/card/CoFounder.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
+import axios from "axios";
 import data from "../card/data";
 import Card from "../card/Card";
 import '../../sass/cofounder.scss';
 
  
 const CoFounder = () => {
-  const [item, setItem] = useState(data);
+  // const [item, setItem] = useState(data);
+  const [item, setItem] = useState([]);
+  function getItem() {
+    axios.get('https://creative.kg/api/founders/').then((res) => {
+      setItem(res.data.results);
+    });
+  }
+  useEffect(() => {
+    getItem();
+  }, []);
+
+
   return (
     <>
       <div className="company-container">
