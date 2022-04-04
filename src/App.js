adc9e9ad29c657702de8b89297910225d82d2f65
ernes raikulov <eraykulov@gmail.com>
2022-04-04 17:51:12 +0600

    add mobile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
-
   Navigation,
   Footer,
   Home,
   About,
   Event,
   JoinUs,
-  Membership
+  Membership,
 } from "./components";
 
-import Resource from './components/Resource';
-import ProjectDetails from './section/Resources/Creative-Project-section/ProjectDetails/ProjectDetails';
+import Resource from "./components/Resource";
+import ProjectDetails from "./section/Resources/Creative-Project-section/ProjectDetails/ProjectDetails";
 import RecommendationDetails from "./section/Resources/Recomendation-section/RecomendationDetails/RecomendationDetails";
 import UtilityDetails from "./section/Resources/Utility/UtilityDetails/UtilityDetails";
 import { IntlProvider } from "react-intl";
@@ -24,29 +23,40 @@ import { LOCALES } from "./i18n/locales";
 import { messages } from "./i18n/messages";
 
 export default function App() {
-    const locale = LOCALES.ENGLISH
-// const [lang,setLang] = useState(LOCALES.ENGLISH)
-    return (
-<IntlProvider messages={messages[locale]}
-      locale={locale}
-      defaultLocale={LOCALES.KYRGYZSTAN}>
+  const locale = LOCALES.ENGLISH;
+  // const [lang,setLang] = useState(LOCALES.ENGLISH)
+  const[menuOpen, setMenuOpen] = useState(false)
   
-    <Navigation />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/event" element={<Event />} />
-      <Route path="/joinUs" element={<JoinUs />} />
-      <Route path="/membership" element={<Membership />} />
-      <Route path="/resource" element={<Resource/>} />
-      <Route path="/resource/creativeprojects/:Id" element ={<ProjectDetails/>}/>
-      <Route path="/resource/recommendations/:Id"element={<RecommendationDetails/>}/>
-      <Route path="/resource/utility/:Id" element={<UtilityDetails/>}/>
-      <Route path="/resource/creativeprojects" element={<Resource/>} />
-      <Route path="/resource/recommendations"element={<Resource/>} />
-      <Route path="/resource/utility" element={<Resource/>} />
-    </Routes>
-    <Footer />
-</IntlProvider>
-    ) 
+  
+  return (
+    <IntlProvider
+      messages={messages[locale]}
+      locale={locale}
+      defaultLocale={LOCALES.KYRGYZSTAN}
+    >
+      <Navigation menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/joinUs" element={<JoinUs />} />
+        <Route path="/membership" element={<Membership />} />
+        <Route path="/resource" element={<Resource />} />
+        <Route
+          path="/resource/creativeprojects/:Id"
+          element={<ProjectDetails />}
+        />
+        <Route
+          path="/resource/recommendations/:Id"
+          element={<RecommendationDetails />}
+        />
+        <Route path="/resource/utility/:Id" element={<UtilityDetails />} />
+        <Route path="/resource/creativeprojects" element={<Resource />} />
+        <Route path="/resource/recommendations" element={<Resource />} />
+        <Route path="/resource/utility" element={<Resource />} />
+      </Routes>
+      <Footer />
+    </IntlProvider>
+  );
 }
--- src/components/card/CardItem.jsx
+++ src/components/card/CardItem.jsx
@@ -1,6 +1,7 @@
 import CardSlider from "../card/CardSlider";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 function CardItem() {
   const [Sliders, setSliders] = useState([]);
   function getSliders() {
