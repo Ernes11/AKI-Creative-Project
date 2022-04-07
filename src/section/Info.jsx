d476338b80a40756263cb3f0acfb4dc7f020ec0f
ernes raikulov <eraykulov@gmail.com>
2022-04-08 01:43:35 +0600

    add mobile

--- src/section/Info.jsx
+++ src/section/Info.jsx
@@ -1,7 +1,5 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "../sass/info.scss";
-
-
 
 function ReadMore({ children }) {
   const text = children;
@@ -11,7 +9,9 @@ function ReadMore({ children }) {
   };
   return (
     <>
-      <p className="text">
+      <p
+        className="text"
+      >
         {isReadMore ? text.slice(0, 150) : text}
         <span onClick={toggleReadMore} className="read-or-hide">
           {isReadMore ? "...Подробнее" : "   Скрыть"}
@@ -29,17 +29,16 @@ function Info() {
 
           <p className="aki-desc">
             <ReadMore>
-            Объединение юридических лиц «Ассоциация Креативных Индустрий»
-            зарегистрирована 19 октября 2021 года. Учредителями Ассоциации
-            выступили 9 компаний из креативного сектора Кыргызстана,
-            представители таких отраслей, как кино, дизайн и мода, коворкинги,
-            архитектура и дизайн интерьера, ивент-агентство, образование и
-            медиа....
+              Объединение юридических лиц «Ассоциация Креативных Индустрий»
+              зарегистрирована 19 октября 2021 года. Учредителями Ассоциации
+              выступили 9 компаний из креативного сектора Кыргызстана,
+              представители таких отраслей, как кино, дизайн и мода, коворкинги,
+              архитектура и дизайн интерьера, ивент-агентство, образование и
+              медиа....
             </ReadMore>
           </p>
-          <hr class="line_horizontal-1"/>
-          <hr class="line_vertical-1"/>
-
+          <hr class="line_horizontal-1" />
+          <hr class="line_vertical-1" />
         </div>
         <div className="aki-row-2">
           <img src="../../images/Creative.png" alt="" />
@@ -53,7 +52,6 @@ function Info() {
         </div>
         <div className="box"></div>
       </div>
-    
     </div>
   );
 }
