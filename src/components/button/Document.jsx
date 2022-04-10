badf86271e391e5569d8f6a05dc2f17dc833cff6
Kasymov Marsel <marselkasymov23@gmail.com>
2022-04-10 15:09:22 +0600

    connecct API for AboutUS Slider

--- src/components/button/Document.jsx
+++ src/components/button/Document.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import "../../sass/document.scss";
 import { FormattedMessage } from 'react-intl';
+import axios from "axios";
 
 function Document() {
   const [show, setShow] = useState(true);
   const [open, setOpen] = useState(true);
+  const [Charter, setCharter] = useState([])
+  const [Policy, setPolicy] = useState([])
+  function getCharter() { axios.get('https://creative.kg/api/reports/')
+      .then(res =>{
+        setCharter (res.data.results)
+      })}
+    useEffect(() => {
+      getCharter();
+    }, []);
 
   return (
     <div className="document">
@@ -26,3 +36,42 @@ function Document() {
 }
 
 export default Document;
+
+
+
+// import React, { useEffect } from "react";
+// import { useState } from "react";
+// import { FormattedMessage } from "react-intl";
+// import "../sass/report.scss";
+// import  axios  from 'axios';
+
+
+// function Report() {
+//   const [reports,setReports] = useState([])
+//   const [show, setShow] = useState(false);
+//   function getReports() { axios.get('https://creative.kg/api/reports/')
+//     .then(res =>{
+//       setReports(res.data.results)
+//     })}
+//   useEffect(() => {
+//     getReports();
+//   }, []);
+//   return (
+//     <div className="report">
+//       <div className="container">
+//         <button onClick={() => setShow(!show)}><FormattedMessage id="Reports"/></button>
+//         {show ? (
+//          <ul>
+//            {reports.map (report => (
+//              <li>
+//                <a target="_blank" href={report.report_text}>{report.report_text}</a>
+//              </li>
+//            ))}
+//           </ul> 
+//         ) : null}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Report;
--- src/section/Carousel.jsx
+++ src/section/Carousel.jsx
@@ -1,5 +1,5 @@
 import "../sass/slider.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Slider from "react-slick";
 import CreativeProjects1 from "../assets/CreativeProjects1.png";
 import CreativeProjects2 from "../assets/CreativeProjects2.png";
@@ -8,10 +8,26 @@ import CreativeBooks from "../assets/CreativeBooks.png";
 import CreativeFilm from "../assets/CreativeFilm.png";
 
 import { MdOutlineKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
+import axios from "axios";
 
-const images = [CreativeProjects1, CreativeProjects2 , CreativeSongs, CreativeBooks, CreativeFilm];
+
 
 function Carousel() {
+  const images2 = [CreativeProjects1, CreativeProjects2 , CreativeSongs, CreativeBooks, CreativeFilm];
+  console.log (images2)
+  const [data,setData] = useState([])
+  const fetchPictures = () => { axios.get('https://creative.kg/api/aboutpictures/')
+  .then(res =>{
+    setData(res.data.results)
+  })}
+  useEffect (()=> {
+    fetchPictures()
+  },[])
+  console.log (data)
+  const images = [data.map ((dat) => dat.picture)]
+console.log (images[0])
+
+
   const NextArrow = ({ onClick }) => {
     return (
       <div className="arrow next" onClick={onClick}>
@@ -33,8 +49,8 @@ function Carousel() {
   const settings = {
     infinite: true,
     lazyLoad: true,
-    speed: 300,
-    slidesToShow: 3,
+    speed: 300, 
+    slidesToShow: 2 ,
     centerMode: true,
     centerPadding: 0,
     nextArrow: <NextArrow />,
@@ -45,7 +61,7 @@ function Carousel() {
   return (
     <div className="Carousel">
       <Slider {...settings}>
-        {images.map((img, idx) => (
+      {images[0].map((img, idx) => (
           <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
             <img src={img} alt={img} />
           </div>
