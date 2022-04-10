e928127387836febcd7efc080559edbdf8511ec8
Kasymov Marsel <marselkasymov23@gmail.com>
2022-04-10 13:49:47 +0600

    fixed resource cards

--- src/section/Resources/Recomendation-section/Recomendation-Slider/SlidesCard.jsx
+++ src/section/Resources/Recomendation-section/Recomendation-Slider/SlidesCard.jsx
@@ -7,8 +7,8 @@ export default function SlidesCard ({img,title,body ,id,type}) {
 
     return(
         <div className="slides_card">
-            <div style={{width:'100%'}}>
-                <img style={{width:'100%', height:'200px'}} src={img} alt="" />
+            <div style={{width:'100%' , height:'50%'}}>
+                <img  style={{width:'100%', height:'100%'}} src={img} alt="" />
             </div>
             <div className="title_container">
                 <h2>{title}</h2>
